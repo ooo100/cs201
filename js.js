de014a4e667a00aaeff872ef1d336e043f32612a
Gayeon <gayeon0090990@gmail.com>
2020-08-13 09:08:40 -0700

    Calculator

---
+++ js.js
@@ -0,0 +1,23 @@
+function AddNumbers(){
+    var number1 = parseInt(prompt("Tell me a number"));
+    var number2 = parseInt(prompt("Tell me a number"));
+    document.getElementById("output").innerHTML = number1 + number2
+}
+
+function SubtractNumbers(){
+    var number1 = parseInt(prompt("Tell me a number"));
+    var number2 = parseInt(prompt("Tell me a number"));
+    document.getElementById("output").innerHTML = number1 - number2
+}
+
+function MultiplyNumbers(){
+    var number1 = parseInt(prompt("Tell me a number"));
+    var number2 = parseInt(prompt("Tell me a number"));
+    document.getElementById("output").innerHTML = number1 * number2
+}
+
+function DivideNumbers(){
+    var number1 = parseInt(prompt("Tell me a number"));
+    var number2 = parseInt(prompt("Tell me a number"));
+    document.getElementById("output").innerHTML = number1 / number2
+}
